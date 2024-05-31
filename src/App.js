748d7a9457e2a736a026b54441ed8e2dcccaf6cb
Dagmara Tur <dagmaratur@Dagmaras-MacBook-Pro.local>
2024-05-31 14:30:43 +0200

    Added main logic for  navigation to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import Hot from './components/Hot';
+import Regular from './components/Regular';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className='nav-menu'>
+          <NavLink to='/hot' className={(navData) => (navData.isActive ? 'active' : '')}>Hot</NavLink>
+          <NavLink to='/regular' className={(navData) => (navData.isActive ? 'active' : '')}>Regular</NavLink>
+        </nav>
+        <Routes>
+          <Route path='/hot' element={<Hot/>}/>
+          <Route path='/regular'element={<Regular/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
